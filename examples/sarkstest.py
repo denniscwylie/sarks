74d4e349061c312a1b2553e0fb3461074250885f
WylieFaehl <denniscwylie@gmail.com>
2018-10-28 17:41:08 -0500

    Added argparse documentation to sarkselect.py and sarkstest.py

--- examples/sarkstest.py
+++ examples/sarkstest.py
@@ -14,23 +14,50 @@ from sarks import Sarks
 
 ## -----------------------------------------------------------------
 parser = argparse.ArgumentParser()
-parser.add_argument('-c', action='store', dest='catfasta', default=None)
-parser.add_argument('-f', action='store', dest='fasta', default=None)
-parser.add_argument('-g', action='store', dest='ginis', default='1.1')
-parser.add_argument('-n', action='store_true', dest='negate', default=False)
-parser.add_argument('-i', action='store', dest='indir', default=None)
-parser.add_argument('-o', action='store', dest='outdir', default=None)
-parser.add_argument('-r', action='store', dest='reps',
-                    type=int, default=100)
-parser.add_argument('-p', action='store_true', dest='prune', default=False)
-parser.add_argument('-s', action='store', dest='scores', default=None)
-parser.add_argument('-z', action='store', dest='nsigma',
-                    type=float, default=4.0)
+parser.add_argument('-i', action='store', dest='indir', default=None,
+                    help='input directory for sarkstest.py is'+
+                         ' output directory from sarkselect.py')
+parser.add_argument('-o', action='store', dest='out', default=None,
+                    help='output directory to be created/overwritten')
+parser.add_argument('-f', action='store', dest='fasta', default=None,
+                    help='input fasta file containing sequences to analyze')
+parser.add_argument('-s', action='store', dest='scores', default=None,
+                    help='input scores tsv file: col1=seqids, col2=numeric scores')
 parser.add_argument('-w', action='store', dest='windows',
-                    default='250,500,1000,2500')
+                    default='250,500,1000,2500',
+                    help='half window width (kappa) for first kernel smoothing pass,' +
+                         ' can supply multiple values using commas (no spaces)')
 parser.add_argument('-l', action='store', dest='spatials',
-                    default='0')
-parser.add_argument('-e', action='store', dest='seed', default=None)
+                    default='0',
+                    help='spatial smoothing length (lambda),' +
+                         ' can supply multiple values using commas (no spaces)' +
+                         ' [default 0]')
+parser.add_argument('-g', action='store', dest='ginis', default='1.1',
+                    help='parameter for calculation of Gini impurity filter (gamma),' +
+                         ' can supply multiple values using commas (no spaces)' +
+                         ' [default 1.1]')
+parser.add_argument('-r', action='store', dest='reps',
+                    type=int, default=100,
+                    help='number R_2 of permutations used to test significance thresholds' +
+                         ' [default 100]')
+parser.add_argument('-z', action='store', dest='nsigma',
+                    type=float, default=4.0,
+                    help='multiple z of standard deviations above mean' +
+                         ' (of maximum smoothed suffix scores obtained after' +
+                         ' randomly permuting scores assigned to sequences)' +
+                         ' defining threshold (Section S2.6, Eq (S24-S25) of paper)' +
+                         ' [default 4.0]')
+parser.add_argument('-p', action='store_true', dest='prune', default=False,
+                    help='(optional flag) remove redundant kmer output'+
+                         ' (Section S2.3 of paper), most useful for small data sets')
+parser.add_argument('-n', action='store_true', dest='negate', default=False,
+                    help='(optional flag) find motifs associated with' +
+                         ' lowest/most negative scores' +
+                         ' instead of highest/most positive scores')
+parser.add_argument('-c', action='store', dest='catfasta', default=None,
+                    help='(optional) path for concatenated fasta file')
+parser.add_argument('-e', action='store', dest='seed', default=None,
+                    help='(optional) seed for random number generator')
 
 parsed = parser.parse_args()
 
