f2822c53561bcb997b76c6fb1430d755304bfe49
Dennis <denniscwylie@gmail.com>
2020-01-23 17:25:30 -0600

    add orientSeqs.R script to examples directory

---
+++ examples/orientSeqs.R
@@ -0,0 +1,16 @@
+orientSeqs <- function(seqs, representative, k=4) {
+    if (length(seqs) == 1) {return(seqs)}
+    names(seqs) <- seqs
+    seqKmers <- kmerCounts(k, seqs, overlap=TRUE)
+    revComps <- sapply(seqs, dnaRevComp)
+    revKmers <- kmerCounts(k, revComps, overlap=TRUE)
+    reverse <- rep(FALSE, length(seqs))
+    repKmers <- as.numeric(seqKmers[representative, ])
+    for (i in 1:length(seqs)) {
+        if (sum(as.numeric(revKmers[i, ]) * repKmers) >
+            sum(as.numeric(seqKmers[i, ]) * repKmers)) {
+            reverse[i] = TRUE
+        }
+    }
+    return(ifelse(reverse, revComps, seqs))
+}
